c3a9d2526b81750553ede65ee25338f18fc620eb
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-03-11 20:48:52 +0800

    generate Program.cs

--- LevenshteinDistance/Program.cs
+++ LevenshteinDistance/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace LevenshteinDistance;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
