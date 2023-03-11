c0bcaec008142a72b0eea9ca34e1da3724bec12e
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-03-11 22:29:24 +0800

    add cache

--- LevenshteinDistance/Levenshtein.cs
+++ LevenshteinDistance/Levenshtein.cs
@@ -1,5 +1,25 @@
 namespace LevenshteinDistance;
 
-public static class Levenshtein {
+public static class Levenshtein
+{
+    private static int Implementation(string s1, string s2, int n1, int n2)
+    {
+        if (n1 == 0) return n2;
+        if (n2 == 0) return n1;
+        if (s1[n1 - 1] == s2[n2 - 1])
+            return Implementation(s1, s2, n1 - 1, n2 - 2);
+        return 1 + Math.Min(
+            Math.Min(
+                 Implementation(s1, s2, n1 - 1, n2),
+                 Implementation(s1, s2, n1, n2 - 1)
+            ),
+            Implementation(s1, s2, n1 - 1, n2 - 1)
+        );
+    }
 
+    public static int Compare(string firstString, string secondString)
+    {
+        int[,] cache = new int[firstString.Length + 1, secondString.Length + 1];
+        return Implementation(firstString, secondString, firstString.Length, secondString.Length);
+    }
 }
--- LevenshteinDistance/Program.cs
+++ LevenshteinDistance/Program.cs
@@ -1,8 +1,26 @@
-﻿namespace LevenshteinDistance;
+﻿using System.Diagnostics;
+
+namespace LevenshteinDistance;
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Stopwatch stopwatch = new();
+        stopwatch.Start();
+        int num;
+        // num = Levenshtein.Compare("apple", "apple");
+        // Console.WriteLine(num);
+        // num = Levenshtein.Compare("apple", "ddapple");
+        // Console.WriteLine(num);
+        // num = Levenshtein.Compare("apple", "appledd");
+        // Console.WriteLine(num);
+        // num = Levenshtein.Compare("apple", "apledd");
+        // Console.WriteLine(num);
+        // num = Levenshtein.Compare("apple", "apledd");
+        // Console.WriteLine(num);
+        num = Levenshtein.Compare("configure", "configure your docs");
+        stopwatch.Stop();
+        Console.WriteLine(num);
+        Console.WriteLine(stopwatch.ElapsedMilliseconds);
     }
 }
